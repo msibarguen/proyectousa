decdb42fec9d19793cf3f5f5586cba5efe8d3ccf
Mario Stivens Ibarguen Serna <msibarguen@hotmail.com>
2021-10-13 19:37:47 -0500

    Proyecto Springboot Base

---
+++ src/test/java/proyectousa/g25/G25ApplicationTests.java
@@ -0,0 +1,13 @@
+package proyectousa.g25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
